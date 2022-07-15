a42cf3dfbbe846cb63324163fefe44280f0bccae
j-reiya-hattori <reiya-hattori@jetb.co.jp>
2022-07-15 11:38:17 +0900

    v2.0.0

--- block-editor-for-wordpress.php
+++ block-editor-for-wordpress.php
@@ -1,38 +1,45 @@
 <?php
 /*
     Plugin Name: Jet Blocks
-    Version: 1.0.0
+    Description: プロックエディタ用のパーツです
+    Version: 2.0.0
+    Plugin URI: https://jetb.co.jp
+    Author: JetB株式会社
+    Author URI: https://jetb.co.jp
     Update URI: jet-blocks
 */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
 
 /**
  * デバック用
  */
 // Latest Releaseの情報を取得するためのエンドポイント
 // https://api.github.com/repos/j-reiya-hattori/block-editor-for-wordpress/releases/latest
-// define( 'MY_PLUGIN_UPDATE_URL', 'https://api.github.com/repos/j-reiya-hattori/block-editor-for-wordpress/releases/latest' );
+define( 'MY_PLUGIN_UPDATE_URL', 'https://api.github.com/repos/j-reiya-hattori/block-editor-for-wordpress/releases/latest' );
 
-// function plugin_update_func( $update, $plugin_data ) {
-//     // GitHub APIを使って、Releaseの最新バージョン情報を取得する
-//     $response = wp_remote_get( MY_PLUGIN_UPDATE_URL );
+function plugin_update_func( $update, $plugin_data ) {
+    // GitHub APIを使って、Releaseの最新バージョン情報を取得する
+    $response = wp_remote_get( MY_PLUGIN_UPDATE_URL );
 
-//     // レスポンスエラー
-//     if( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) {
-//         return $update;
-//     }
+    // レスポンスエラー
+    if( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) {
+        return $update;
+    }
 
-//     // 最新バージョン、zipファイルパッケージのURLを取得
-//     $response_body = json_decode( wp_remote_retrieve_body( $response ), true );
-//     $new_version   = isset( $response_body['tag_name'] ) ? $response_body['tag_name'] : null;
-//     $package       = isset( $response_body['assets'][0]['browser_download_url'] ) ? $response_body['assets'][0]['browser_download_url'] : null;
+    // 最新バージョン、zipファイルパッケージのURLを取得
+    $response_body = json_decode( wp_remote_retrieve_body( $response ), true );
+    $new_version   = isset( $response_body['tag_name'] ) ? $response_body['tag_name'] : null;
+    $package       = isset( $response_body['assets'][0]['browser_download_url'] ) ? $response_body['assets'][0]['browser_download_url'] : null;
 
-//     return array(
-//         'version'     => $plugin_data['Version'], // 現在のバージョン
-//         'new_version' => $new_version,            // 最新のバージョン
-//         'package'     => $package,                // zipファイルパッケージのURL
-//     );
-// }
-// add_filter( 'update_plugins_jet-blocks', 'plugin_update_func', 10, 2 );
+    return array(
+        'version'     => $plugin_data['Version'], // 現在のバージョン
+        'new_version' => $new_version,            // 最新のバージョン
+        'package'     => $package,                // zipファイルパッケージのURL
+        'url'         => 'https://jetb.co.jp',
+    );
+}
+add_filter( 'update_plugins_jet-blocks', 'plugin_update_func', 10, 2 );
 
 
 
@@ -47,8 +54,10 @@ function register_menu_page(){
 }
 
 function menu_page(){
-    $version = '1.0.0';
+    $version = '2.0.0';
     echo "<h2>Jet Blocks設定</h2>";
     echo "<p>version: {$version}</p>";
-    echo "<p>現在のバージョンは<b>{$version}</b>です</p>";  
+    echo "<p>現在のバージョンは<b>{$version}</b>です</p>";
+    echo "<h3>更新情報</h3>";
+    echo "<p>2022年7月15日：　セキュリティ強化しました</p>";
 }
